dd2c5a68f238d801dee2e04485e899a5bff41507
unknown <lemon@codeshaper.tech>
2022-05-23 14:39:21 +0600

    initial setup done

---
+++ resources/views/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LP1 - Landing Page 1</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Small CSS to Hide elements at 1520px size -->
+    <style>
+        @media(max-width:1920px){
+            .left-svg{
+                display:none;
+            }
+        }
+        /* small css for the mobile nav close */
+        #nav-mobile-btn.close span:first-child{
+            transform: rotate(45deg);
+            top: 4px;
+            position: relative;
+            background:#a0aec0;
+        }
+        #nav-mobile-btn.close span:nth-child(2){
+            transform: rotate(-45deg);
+            margin-top: 0px;
+            background:#a0aec0;
+        }
+    </style>
+</head>
+
+<body class="antialiased overflow-x-hidden">
+@yield('content')
+
+
+</body>
+</html>
